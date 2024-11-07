52af3b49355ec295811ae7db1f54b0c3e755d8e8
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-11-07 17:12:47 +0000

    Modified main branch

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main { 
     public static void main(String[] args) { 
-    System.out.println("Main Version Update"); 
-    System.out.println("One");
-    System.out.println("Two");
+    System.out.println("Main Version Update");
 }
 }
