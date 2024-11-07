2ad11951a5e1d358241e1dcea8ece509ea1f6e66
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-11-07 16:51:33 +0000

    added 1 & 2 in main

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) { 
     System.out.println("Feature Version"); 
     System.out.println("Main Version Update"); 
-    System.out.println(One);
+    System.out.println("One");
     System.out.println("Two");
 }
 }
