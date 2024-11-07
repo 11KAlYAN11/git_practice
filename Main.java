0c068a8bbf4895164cd0776324469646a3808022
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-11-07 16:51:06 +0000

    added 1 & 2 in main

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main { 
     public static void main(String[] args) { 
     System.out.println("Main Version Update"); 
+    System.out.println(One);
+    System.out.println("Two");
 }
 }
