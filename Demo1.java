c019c3b563b429d4c69ab7bc562873b2d60fcf6c
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-11-09 04:53:17 +0000

    Hi from  mina Nice!

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,6 @@
 public class Demo1 {
     public static void main(String [] args) {
-        System.out.println("Hi I/'m from feature (merge conflit) ")
+    System.out.println("Hi I/'m from feature (merge conflit) ")
+    System.out.println("Hi, I/'m from Main!");
     }
 }
